8150f924607b1736da9996860bb65d979d7104fd
Melissa-Ann-Giuliani <melissa.a.giuliani@gmail.com>
2026-04-28 23:32:48 -0300

    Cambios realizados según Módulo 3

--- README.md
+++ README.md
@@ -32,5 +32,48 @@ Estos valores no se tocarán todavía
 También se creó un switch en el main.js para tomar el Tipo de Vivienda a través de un prompt, y cambiar en base al valor ingresado, el factor de multiplicación.
 Luego se validó si:
 * el factor de multplicación posee un valor superior a 1.000
-* si los metros cuadrados ingresados en un prompt se pueden convertir a un valor entero.
+* los metros cuadrados ingresados en un prompt se pueden convertir a un valor entero.
 
+## Módulo 3
+Se creó variables.js, con la información de tipo y factor de multiplicación respectivos para cada tipo de vivienda y ubicación. Esta informacion siendo guardada en arreglos de objetos. 
+
+Al main.js se lo modifica a modo que busque dentro de cada arreglo, la coincidencia con lo que el usuario ingrese a través de un prompt, y al encontrarlo, guarde el objeto. Al objeto guardado para la ubicación deseada, y el tipo de vivienda deseada, se utilizan sus factores de multipllicación respectivos para el cálculo final.
+
+```html
+let propertyFM;
+let i = 0;
+let propertyInput = prompt("Ingrese tipo de vivienda:\n1- Casa\n2- P.H.\n3- Dto. Edificio\n4- Barrio Privado\n5- Oficina\n6- Local Comercial\n7-Depósito logística\n ");
+
+while(i<propertyData.length&&propertyData[i].tipo!==propertyInput){
+    i++;
+}
+
+if(i<propertyData.length){
+    propertyFM = propertyData[i];
+}else {
+    alert("Valor de vivienda incorrecto!");
+}
+
+let locationFM;
+let j = 0;
+let locationInput = prompt("Ingrese su Ubicación:\n1- CABA\n2- Tandil\n3- Costa Atlántica\n4- Patagonia Argentina\n");
+
+while(j<locationData.length&&locationData[j].tipo!==locationInput){
+    j++;
+}
+
+if(j<locationData.length){
+    locationFM = locationData[j];
+}else{
+    alert("Valor de ubicación incorrecto!");
+}
+
+if(propertyFM.factor>1.000 && locationFM.factor>1.000 && parseInt(totalM2))
+{
+    let totalAmmount = propertyFM.factor*locationFM.factor*m2BaseCost*totalM2;
+    console.log(`Resultado de la multiplicación $ ${totalAmmount.toFixed(4)}`);
+}
+else {
+    console.warn("El factor de multiplicación no es superior a 1000.00 o los metros cuadrados no se pueden convertir a entero");
+}
+```
--- js/main.js
+++ js/main.js
@@ -1,48 +1,39 @@
-const m2_base_cost = 1.16;
+const m2BaseCost = 1.16;
 
-let total_m2 = prompt("Ingresa los metros cuadrados de la vivienda: ");
+let totalM2 = prompt("Ingresa los metros cuadrados de la vivienda: ");
 
-let fm;
+let propertyFM;
+let i = 0;
+let propertyInput = prompt("Ingrese tipo de vivienda:\n1- Casa\n2- P.H.\n3- Dto. Edificio\n4- Barrio Privado\n5- Oficina\n6- Local Comercial\n7-Depósito logística\n ");
 
-let building_type = prompt("Ingrese un número para tipo de vivienda:\n1- Casa\n2- P.H.\n3- Dto. Edificio\n4- Barrio Privado\n5- Oficina\n6- Local Comercial\n7-Depósito logística\n");
-
-switch(building_type){
-    case '1': {
-        /* fm=document.getElementById("property").value; */
-        fm = 1.009;
-        break;
-    }
-    case '2': {
-        fm = 1.005;
-        break;
-    }
-    case '3': {
-        fm=1.002;
-        break;
-    }
-    case '4': {
-        fm = 1.019;
-        break;
-    }
-    case '5': {
-        fm = 1.039;
-        break;
-    }
-    case '6': {
-        fm = 1.041;
-        break;
-    }
-    case '7': {
-        fm = 1.092;
-        break;
-    }
+while(i<propertyData.length&&propertyData[i].tipo!==propertyInput){
+    i++;
 }
 
+if(i<propertyData.length){
+    propertyFM = propertyData[i];
+}else {
+    alert("Valor de vivienda incorrecto!");
+}
 
-if(fm>1.000 && parseInt(total_m2))
+let locationFM;
+let j = 0;
+let locationInput = prompt("Ingrese su Ubicación:\n1- CABA\n2- Tandil\n3- Costa Atlántica\n4- Patagonia Argentina\n");
+
+while(j<locationData.length&&locationData[j].tipo!==locationInput){
+    j++;
+}
+
+if(j<locationData.length){
+    locationFM = locationData[j];
+}else{
+    alert("Valor de ubicación incorrecto!");
+}
+
+if(propertyFM.factor>1.000 && locationFM.factor>1.000 && parseInt(totalM2))
 {
-    let total_ammount = m2_base_cost*total_m2*fm;
-    console.log(`Resultado de la multiplicación $ ${total_ammount.toFixed(4)}`);
+    let totalAmmount = propertyFM.factor*locationFM.factor*m2BaseCost*totalM2;
+    console.log(`Resultado de la multiplicación $ ${totalAmmount.toFixed(4)}`);
 }
 else {
     console.warn("El factor de multiplicación no es superior a 1000.00 o los metros cuadrados no se pueden convertir a entero");
